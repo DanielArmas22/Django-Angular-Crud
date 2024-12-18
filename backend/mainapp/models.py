38bee67edd93f709823a80602cb62b6f9de73506
DanielArmas22 <106291412+DanielArmas22@users.noreply.github.com>
2024-12-18 15:17:22 -0500

    Configuracion del backend, creacion de la tabla usuario

---
+++ backend/mainapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class Usuario(AbstractUser):
+  dni = models.CharField(max_length=8, unique=True)
+  telefono = models.CharField(max_length=9)
+  
